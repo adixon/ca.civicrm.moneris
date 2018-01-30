1461cf579f4afd2f44601d47b9988cda802ee513
Mathieu Lutfy <mathieu@symbiotic.coop>
2018-01-30 13:38:07 -0500

    Fixes #3: CiviCRM 4.7 support

--- sql/install.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO civicrm_managed (module, name, entity_type, entity_id) VALUES ('ca.civicrm.moneris','Moneris','PaymentProcessorType',(SELECT id FROM civicrm_payment_processor_type where name = 'Moneris'));
