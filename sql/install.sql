faf7269703c684d4dd8397302ca1337a9735343c
adixon <alan.g.dixon@gmail.com>
2018-09-04 10:33:00 -0400

    no longer requires install sql

--- sql/install.sql
+++ sql/install.sql
@@ -1,1 +1,1 @@
-INSERT INTO civicrm_managed (module, name, entity_type, entity_id) VALUES ('ca.civicrm.moneris','Moneris','PaymentProcessorType',(SELECT id FROM civicrm_payment_processor_type where name = 'Moneris'));
+-- INSERT INTO civicrm_managed (module, name, entity_type, entity_id) VALUES ('ca.civicrm.moneris','Moneris','PaymentProcessorType',(SELECT id FROM civicrm_payment_processor_type where name = 'Moneris'));
