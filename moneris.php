5bb49ac53c24c98526723c10c3312a432600c3b9
Alan Dixon <alan.g.dixon@gmail.com>
2023-11-13 18:23:40 +0000

    civix upgrade

--- moneris.php
+++ moneris.php
@@ -30,21 +30,6 @@ function moneris_civicrm_enable() {
 }
 
 /**
- * Implementation of hook_civicrm_upgrade
- *
- * @param $op string, the type of operation being performed; 'check' or 'enqueue'
- * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
- *
- * @return mixed  based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
- *                for 'enqueue', returns void
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function moneris_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return;
-}
-
-/**
  * Implementation of hook_civicrm_managed
  *
  * Generate a list of entities to create/deactivate/delete when this module
